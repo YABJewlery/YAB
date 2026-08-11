f64737a5f3cfb366b8a8d4114bd2283066a88993
YABJewlery <yabpratas@gmail.com>
2026-08-11 15:31:23 -0300

    Refactor products array initialization

--- products.js
+++ products.js
@@ -1,744 +1,703 @@
 const products = [
 
 {
-    id:1,
+id:1,
 
-    name:"Anel Ondinhas",
+name:"Anel Ondinhas",
 
-    category:"Anéis",
+category:"Anéis",
 
-    badge:"",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:39.90,
+        price:39.90,
 
-            image:"anelondinhas.png",
+        image:"anelondinhas.png",
 
-            stock:false
-        },
+        stock:false
+    },
 
-        {
-            material:"Prata",
+    {
+        material:"Prata",
 
-            price:36.90,
+        price:36.90,
 
-            image:"anelondinhas1.png",
+        image:"anelondinhas1.png",
 
-            stock:false
-        }
+        stock:false
+    }
 
-    ]
-
-},
-
-
-
-{
-    id:2,
-
-    name:"Anel Falange",
-
-    category:"Anéis",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:49.90,
-
-            image:"anelfalanger.png",
-
-            stock:true
-        },
-
-        {
-            material:"Prata",
-
-            price:0,
-
-            image:"anelfalange.png",
-
-            stock:false
-        }
-
-    ]
-
-},
-
-
-
-{
-    id:3,
-
-    name:"Anel Canelado",
-
-    category:"Anéis",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:49.90,
-
-            image:"anelcanelado1.png",
-
-            stock:true
-        },
-
-
-        {
-            material:"Prata",
-
-            price:0,
-
-            image:"anelcanelado.png",
-
-            stock:true
-        }
-
-    ]
-
-},
-
-
-
-{
-    id:4,
-
-    name:"Anel Abauladas",
-
-    category:"Anéis",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:59.90,
-
-            image:"anelpontasa.png",
-
-            stock:false
-        },
-
-
-        {
-            material:"Prata",
-
-            price:0,
-
-            image:"anelpontas.png",
-
-            stock:false
-        }
-
-    ]
-
-},
-
-
-
-{
-    id:5,
-
-    name:"Anel Quadrado",
-
-    category:"Anéis",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:54.90,
-
-            image:"quadrado1.png",
-
-            stock:true
-        },
-
-
-        {
-            material:"Prata",
-
-            price:48.90,
-
-            image:"quadrado.png",
-
-            stock:false
-        }
-
-    ]
-
-},
-
-
-
-{
-    id:6,
-
-    name:"Anel Giratorio",
-
-    category:"Anéis",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:64.90,
-
-            image:"anelgira.png",
-
-            stock:true
-        },
-
-
-        {
-            material:"Prata",
-
-            price:57.90,
-
-            image:"anelgiral.png",
-
-            stock:true
-        }
-
-    ]
-
-},
-
-
-
-{
-    id:7,
-
-    name:"Anel Meia Lua",
-
-    category:"Anéis",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:53.90,
-
-            image:"anelmeial.png",
-
-            stock:true
-        },
-
-
-        {
-            material:"Prata",
-
-            price:46.90,
-
-            image:"anelmeia.png",
-
-            stock:true
-        }
-
-    ]
-
-},
-
-
-
-{
-    id:8,
-
-    name:"Anel Placa liso",
-
-    category:"Anéis",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:66.90,
-
-            image:"anelplacalisa1.png",
-
-            stock:true
-        },
-
-
-        {
-            material:"Prata",
-
-            price:58.90,
-
-            image:"anelplacalisa.png",
-
-            stock:true
-        }
-
-    ]
-
-},
-
-
-
-{
-    id:9,
-
-    name:"Brinco Argola Torcido",
-
-    category:"Brincos",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:51.90,
-
-            image:"argolatorc1.png",
-
-            stock:false
-        },
-
-
-        {
-            material:"Prata",
-
-            price:46.90,
-
-            image:"argolatorc.png",
-
-            stock:false
-        }
-
-    ]
-
-},
-
-
-
-{
-    id:10,
-
-    name:"Brinco Gotinha Baby",
-
-    category:"Brincos",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:34.90,
-
-            image:".png",
-
-            stock:false
-        },
-
-
-        {
-            material:"Prata",
-
-            price:29.90,
-
-            image:".png",
-
-            stock:false
-        }
-
-    ]
-
+]
 },
 
 {
-    id:11,
+id:2,
 
-    name:"Brinco Gota Riscado",
+name:"Anel Falange",
 
-    category:"Brincos",
+category:"Anéis",
 
-    badge:"",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:49.90,
+        price:49.90,
 
-            image:"brincogotariscado1.png",
+        image:"anelfalanger.png",
 
-            stock:false
-        },
+        stock:true
+    },
 
+    {
+        material:"Prata",
 
-        {
-            material:"Prata",
+        price:0,
 
-            price:38.90,
+        image:"anelfalange.png",
 
-            image:"brincogotariscado.png",
+        stock:false
+    }
 
-            stock:true
-        }
-
-    ]
-
-},
-
-
-{
-    id:12,
-
-    name:"Brinco Retangulo",
-
-    category:"Brincos",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:45.90,
-
-            image:"retanguloduplo.png",
-
-            stock:false
-        },
-
-
-        {
-            material:"Prata",
-
-            price:40.90,
-
-            image:"retanguloduplo1.png",
-
-            stock:true
-        }
-
-    ]
-
-},
-{
-    id:13,
-
-    name:"Choker fita",
-
-    category:"Colares",
-
-    badge:"",
-
-    variants:[
-
-        {
-            material:"Ouro",
-
-            price:89.90,
-
-            image:"chokerfita1.png",
-
-            stock:true
-        },
-
-
-        {
-            material:"Prata",
-
-            price:0,
-
-            image:"chokerfita.png",
-
-            stock:true
-        }
-
-    ]
-
+]
 },
 
 {
-    id:14,
+id:3,
 
-    name:"Colar Coração Liso",
+name:"Anel Canelado",
 
-    category:"Colares",
+category:"Anéis",
 
-    badge:"",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:49.90,
+        price:49.90,
 
-            image:"colarcoracaoliso.png",
+        image:"anelcanelado1.png",
 
-            stock:true
-        },
+        stock:true
+    },
 
 
-        {
-            material:"Prata",
+    {
+        material:"Prata",
 
-            price:0,
+        price:0,
 
-            image:"colarcoracaol.png",
+        image:"anelcanelado.png",
 
-            stock:false
-        }
+        stock:true
+    }
 
-    ]
-
+]
 },
 
 {
-    id:15,
+id:4,
 
-    name:"Brinco Argola Torcido Pequena",
+name:"Anel Abauladas",
 
-    category:"Brincos",
+category:"Anéis",
 
-    badge:"Novidade",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:38.90,
+        price:59.90,
 
-            image:".png",
+        image:"anelpontasa.png",
 
-            stock:false
-        },
+        stock:false
+    },
 
 
-        {
-            material:"Prata",
+    {
+        material:"Prata",
 
-            price:33.90,
+        price:0,
 
-            image:".png",
+        image:"anelpontas.png",
 
-            stock:false
-        }
+        stock:false
+    }
 
-    ]
-
+]
 },
 
 {
-    id:16,
+id:5,
 
-    name:"Colar Octagonal Verde",
+name:"Anel Quadrado",
 
-    category:"Colares",
+category:"Anéis",
 
-    badge:"",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:75.90,
+        price:54.90,
 
-            image:"colaroctagonalv1.png",
+        image:"quadrado1.png",
 
-            stock:false
-        },
+        stock:true
+    },
 
 
-        {
-            material:"Prata",
+    {
+        material:"Prata",
 
-            price:69.90,
+        price:48.90,
 
-            image:"colaroctagonalv.png",
+        image:"quadrado.png",
 
-            stock:true
-        }
+        stock:false
+    }
 
-    ]
-
+]
 },
 
 {
-    id:17,
+id:6,
 
-    name:"Colar Ponto de Luz",
+name:"Anel Giratorio",
 
-    category:"Colares",
+category:"Anéis",
 
-    badge:"",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:59.90,
+        price:64.90,
 
-            image:"pontodeluz1.png",
+        image:"anelgira.png",
 
-            stock:false
-        },
+        stock:true
+    },
 
 
-        {
-            material:"Prata",
+    {
+        material:"Prata",
 
-            price:54.90,
+        price:57.90,
 
-            image:"pontodeluz.png",
+        image:"anelgiral.png",
 
-            stock:true
-        }
+        stock:true
+    }
 
-    ]
-
+]
 },
 
 {
-    id:18,
+id:7,
 
-    name:"Colar Veneziana",
+name:"Anel Meia Lua",
 
-    category:"Colares",
+category:"Anéis",
 
-    badge:"",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:59.90,
+        price:53.90,
 
-            image:"venezianabol1.png",
+        image:"anelmeial.png",
 
-            stock:false
-        },
+        stock:true
+    },
 
 
-        {
-            material:"Prata",
+    {
+        material:"Prata",
 
-            price:0,
+        price:46.90,
 
-            image:"venezianabol.png",
+        image:"anelmeia.png",
 
-            stock:false
-        }
+        stock:true
+    }
 
-    ]
-
+]
 },
 
 {
-    id:19,
+id:8,
 
-    name:"Pulseira Coração",
+name:"Anel Placa liso",
 
-    category:"Pulseiras",
+category:"Anéis",
 
-    badge:"",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:48.90,
+        price:66.90,
 
-            image:"coracaoverde.png",
+        image:"anelplacalisa1.png",
 
-            stock:true
-        },
+        stock:true
+    },
 
 
-        {
-            material:"Prata",
+    {
+        material:"Prata",
 
-            price:41.90,
+        price:58.90,
 
-            image:"coracaoverde1.png",
+        image:"anelplacalisa.png",
 
-            stock:false
-        }
+        stock:true
+    }
 
-    ]
-
+]
 },
 
 {
-    id:20,
+id:9,
 
-    name:"Pulseira de Mão",
+name:"Brinco Argola Torcido",
 
-    category:"Pulseiras",
+category:"Brincos",
 
-    badge:"",
+badge:"",
 
-    variants:[
+variants:[
 
-        {
-            material:"Ouro",
+    {
+        material:"Ouro",
 
-            price:44.90,
+        price:51.90,
 
-            image:"pulseirarisquinhoam.png",
+        image:"argolatorc1.png",
 
-            stock:false
-        },
+        stock:false
+    },
 
 
-        {
-            material:"Prata",
+    {
+        material:"Prata",
 
-            price:40.90,
+        price:46.90,
 
-            image:"pulseirarisquinhoam1.png",
+        image:"argolatorc.png",
 
-            stock:true
-        }
+        stock:false
+    }
 
-    ]
+]
+},
 
+{
+id:10,
+
+name:"Brinco Gotinha Baby",
+
+category:"Brincos",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:34.90,
+
+        image:".png",
+
+        stock:false
+    },
+
+
+    {
+        material:"Prata",
+
+        price:29.90,
+
+        image:".png",
+
+        stock:false
+    }
+
+]
+},
+
+{
+id:11,
+
+name:"Brinco Gota Riscado",
+
+category:"Brincos",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:49.90,
+
+        image:"brincogotariscado1.png",
+
+        stock:false
+    },
+
+
+    {
+        material:"Prata",
+
+        price:38.90,
+
+        image:"brincogotariscado.png",
+
+        stock:true
+    }
+
+]
+},
+
+{
+id:12,
+
+name:"Brinco Retangulo",
+
+category:"Brincos",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:45.90,
+
+        image:"retanguloduplo.png",
+
+        stock:false
+    },
+
+
+    {
+        material:"Prata",
+
+        price:40.90,
+
+        image:"retanguloduplo1.png",
+
+        stock:true
+    }
+
+]
+},
+{
+id:13,
+
+name:"Choker fita",
+
+category:"Colares",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:89.90,
+
+        image:"chokerfita1.png",
+
+        stock:true
+    },
+
+
+    {
+        material:"Prata",
+
+        price:0,
+
+        image:"chokerfita.png",
+
+        stock:true
+    }
+
+]
+},
+
+{
+id:13,
+
+name:"Colar Coração Liso",
+
+category:"Colares",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:49.90,
+
+        image:"colarcoracaoliso.png",
+
+        stock:true
+    },
+
+
+    {
+        material:"Prata",
+
+        price:0,
+
+        image:"colarcoracaol.png",
+
+        stock:false
+    }
+
+]
+},
+
+{
+id:14,
+
+name:"Brinco Argola Torcido Pequena",
+
+category:"Brincos",
+
+badge:"Novidade",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:38.90,
+
+        image:".png",
+
+        stock:false
+    },
+
+
+    {
+        material:"Prata",
+
+        price:33.90,
+
+        image:".png",
+
+        stock:false
+    }
+
+]
+},
+
+{
+id:14,
+
+name:"Colar Octagonal Verde",
+
+category:"Colares",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:75.90,
+
+        image:"colaroctagonalv1.png",
+
+        stock:false
+    },
+
+
+    {
+        material:"Prata",
+
+        price:69.90,
+
+        image:"colaroctagonalv.png",
+
+        stock:true
+    }
+
+]
+},
+
+{
+id:14,
+
+name:"Colar Ponto de Luz",
+
+category:"Colares",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:59.90,
+
+        image:"pontodeluz1.png",
+
+        stock:false
+    },
+
+
+    {
+        material:"Prata",
+
+        price:54.90,
+
+        image:"pontodeluz.png",
+
+        stock:true
+    }
+
+]
+},
+
+{
+id:15,
+
+name:"Colar Veneziana",
+
+category:"Colares",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:59.90,
+
+        image:"venezianabol1.png",
+
+        stock:false
+    },
+
+
+    {
+        material:"Prata",
+
+        price:0,
+
+        image:"venezianabol.png",
+
+        stock:false
+    }
+
+]
+},
+
+{
+id:16,
+
+name:"Pulseira Coração",
+
+category:"Pulseiras",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:48.90,
+
+        image:"coracaoverde.png",
+
+        stock:true
+    },
+
+
+    {
+        material:"Prata",
+
+        price:41.90,
+
+        image:"coracaoverde1.png",
+
+        stock:false
+    }
+
+]
+},
+
+{
+id:17,
+
+name:"Pulseira de Mão",
+
+category:"Pulseiras",
+
+badge:"",
+
+variants:[
+
+    {
+        material:"Ouro",
+
+        price:44.90,
+
+        image:"pulseirarisquinhoam.png",
+
+        stock:false
+    },
+
+
+    {
+        material:"Prata",
+
+        price:40.90,
+
+        image:"pulseirarisquinhoam1.png",
+
+        stock:true
+    }
+
+]
 }
 
-
 ];
-
 
 console.log(
 "Produtos carregados:",
