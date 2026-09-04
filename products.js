6897ccdc96d10b6b2937e9dae0dd4064b89524e7
YABJewlery <yabpratas@gmail.com>
2026-09-03 22:46:56 -0300

    Update product images and stock status
    Updated product images for several items and ensured stock status is accurate.

--- products.js
+++ products.js
@@ -8,13 +8,13 @@ const products = [
             {
                 material: "Ouro",
                 price: 89.90,
-                image: "",
+                image: "cholorisouro.png",
                 stock: false
             },
             {
                 material: "Prata",
                 price: 79.90,
-                image: "",
+                image: "cholorisprata.png",
                 stock: false
             }
         ]
